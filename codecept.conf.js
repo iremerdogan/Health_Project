f2d0b7db3f42641a1342d6c5c7c5f9d2be893a60
İrem Nur Erdoğan <iremnurerdogan@protonmail.com>
2026-02-14 00:11:33 +0300

    Initial project setup

---
+++ codecept.conf.js
@@ -0,0 +1,29 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: './tests/*_test.js',
+  output: './output',
+  helpers: {
+    WebDriver: {
+      url: 'https://o3.openmrs.org/openmrs/spa/login#',
+      browser: 'firefox',
+      geckoDriver: './node_modules/.bin/geckodriver'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  plugins: {
+    htmlReporter: {
+      enabled: true
+    }
+  },
+  name: 'Health_Project'
+}
